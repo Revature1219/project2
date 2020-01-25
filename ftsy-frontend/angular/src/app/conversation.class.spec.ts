6c5d5cbca976e3f26170d25bb001c795b0a8ba28
allgoodlm <allgood.lm@gmail.com>
2020-01-24 19:08:05 -0600

    Hopefully, the last commit to fix the directory names.

--- ftsy-frontend/angular/src/app/conversation.class.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Conversation } from './conversation.class';
-
-describe('Conversation', () => {
-  it('should create an instance', () => {
-    expect(new Conversation()).toBeTruthy();
-  });
-});
