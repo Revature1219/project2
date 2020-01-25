592be5a6702dd96fbca747bbc3b8b95afcd753d5
allgoodlm <allgood.lm@gmail.com>
2020-01-24 19:10:44 -0600

    Hopefully, THIS is the last push to fix the directory names.

---
+++ itsy-frontend/angular/src/app/item.class.ts
@@ -0,0 +1,21 @@
+import { Seller } from './seller.class';
+import { Review } from './review.class';
+
+export class Item {
+    
+    // private int id;
+    id : number;
+    // private String name;
+    name : string;
+    // private double price;
+    price : number;
+    // private String details;
+    details : string;
+	// @OneToOne(fetch = FetchType.LAZY)
+    // private Seller seller;
+    seller : Seller;
+	// @OneToMany(fetch = FetchType.LAZY)
+    // private List<Review> reviews;
+    reviews : Review[];
+    
+}
